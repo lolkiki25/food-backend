66dade7782e76fb2c1648bc9117e0bd355639e3c
lolkiki25 <25LP2625@nest.edu.mn>
2025-10-07 12:21:59 +0800

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port = 3000;
+const port = 3300;
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +11,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
+app.post("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+// foods create-foods {categories: main dish, something, appitizer
